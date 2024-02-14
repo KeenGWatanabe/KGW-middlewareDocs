5d44f6eec6a055d38ff342bb78d5965bed7e832e
60936 <studio34pl@gmail.com>
2024-02-14 20:36:57 +0800

    r

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const myLogger = require('./middleware/myLogger');
+const requestTime = require('./middleware/requestTime');
+//middleware 
 
+app.use(myLogger);
+app.use(requestTime);
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let responseText = 'Hello World!<br>'
+  responseText += `<small>requested at: ${req.requestTime}</small>`;
+  res.send(responseText);
 });
 
-app.listen(3000, () => {
+app.listen(3000), () => {
   console.log('Example app listening on port 3000!');
-});
+};
---
+++ middleWare/myLogger.js
@@ -0,0 +1,6 @@
+
+const myLogger = function (req, res, next) {
+   console.log('logged'); 
+next()};
+
+module.exports = myLogger;
